918724f715a29036f0843ddb2777a8ede66b8a57
danial <dananu986@gmail.com>
2024-06-26 07:44:49 +0500

    Request class

--- public/index.php
+++ public/index.php
@@ -3,4 +3,6 @@
 require_once __DIR__ . '/../vendor/autoload.php'; // Adjust the path as necessary
 use App\Maish;
 (new Maish())->text();
-dd("dsa");
+$request = \Danial\PhpFramework\Http\Request::createFromGlobals();
+
+dd($request);
